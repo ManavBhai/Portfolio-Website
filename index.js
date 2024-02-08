e5ee26cd6b6bc4e1ef3f63186ece91e180fce0ce
Manav Thakkar <147579754+ManavBhai@users.noreply.github.com>
2024-02-08 20:31:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+function openGitHub() {
+    window.open("https://github.com/ManavBhai?tab=repositories", "_blank");
+}
+function openLinkedIn() {
+    window.open("https://www.linkedin.com/in/manav-thakkar-26810b233?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app","_blank");
+}
+function openInsta() {
+    window.open("https://www.instagram.com/_thakkar.manav_","_blank");
+}
